dce03b79b983410d9384c9b1bc86e25f85b2c491
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2022-03-05 22:53:34 +0200

    adding main and making a clock rate

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
 import math
+
 pygame.init()
 
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+main()
